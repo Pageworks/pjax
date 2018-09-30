b39f7b1ebcf4b805f8eb9244335ddc97ee83caaa
Kyle Andrews <andrews@gamesbykyle.com>
2018-09-30 10:36:44 -0400

    Fixes parse-options linting issues

--- scripts/lib/parse-options.ts
+++ scripts/lib/parse-options.ts
@@ -1,15 +1,17 @@
 import Pjax from '../globals';
 
-export default function parseOptions(options?: Pjax.IOptions){
-    options                         = options || {};
-    options.elements                = options.elements || 'a[href]';
-    options.selectors               = options.selectors || ['title', '.js-pjax'];
-    options.switches                = options.switches || {};
-    options.history                 = (typeof options.history === undefined) ? true : options.history;
-    options.scrollTo                = (typeof options.scrollTo === undefined) ? 0 : options.scrollTo;
-    options.cacheBust               = (typeof options.cacheBust === undefined) ? true : options.cacheBust;
-    options.debug                   = options.debug || false;
-    options.timeout                 = options.timeout || 0;
+let parsedOptions: Pjax.IOptions;
 
-    return options;
+export default function parseOptions(options?: Pjax.IOptions){
+    parsedOptions                         = options || {};
+    parsedOptions.elements                = options.elements || 'a[href]';
+    parsedOptions.selectors               = options.selectors || ['title', '.js-pjax'];
+    parsedOptions.switches                = options.switches || {};
+    parsedOptions.history                 = (typeof options.history === undefined) ? true : options.history;
+    parsedOptions.scrollTo                = (typeof options.scrollTo === undefined) ? 0 : options.scrollTo;
+    parsedOptions.cacheBust               = (typeof options.cacheBust === undefined) ? true : options.cacheBust;
+    parsedOptions.debug                   = options.debug || false;
+    parsedOptions.timeout                 = options.timeout || 0;
+
+    return parsedOptions;
 }
