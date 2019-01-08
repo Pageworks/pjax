d56b22483974cc84befd904deca245254bc5cee7
Kyle Andrews <kylea@page.works>
2019-01-08 09:03:35 -0500

    Adds Custom Events polyfill for IE 11

--- _released/pjax-1.0.8.js
+++ _released/pjax-1.0.8.js
@@ -211,6 +211,18 @@ exports.default = (function () {
             };
         }());
     }
+    (function () {
+        if (typeof window.CustomEvent === "function")
+            return false;
+        function CustomEvent(event, params) {
+            params = params || { bubbles: false, cancelable: false, detail: undefined };
+            var evt = document.createEvent('CustomEvent');
+            evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
+            return evt;
+        }
+        CustomEvent.prototype = window.Event.prototype;
+        window.CustomEvent = CustomEvent;
+    })();
 });
 
 },{}],8:[function(require,module,exports){
